2a6c72b610d493025a4b5f651483dee70d379828
sahil8128 <sahilgulati651@gmail.com>
2020-07-02 01:21:50 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, VERSION } from '@angular/core';
-import { FormGroup,FormControl } from '@angular/forms';
+import { FormGroup,FormControl,FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'my-app',
@@ -8,15 +8,76 @@ import { FormGroup,FormControl } from '@angular/forms';
 })
 export class AppComponent  {
 
-  registrationForm = new FormGroup({
-    user_name : new FormControl('Sahil'),
-    user_password  : new FormControl(''),
-    confirm_password : new FormControl(''),
-    user_address : new FormGroup({
-    user_city : new FormControl(''),
-    user_state  : new FormControl(''),
-    user_postCode : new FormControl(''),
+  constructor(private fb : FormBuilder){}
+
+
+  // ----Using FormBuilder method to create model for the Form---->
+
+  registrationForm = this.fb.group({
+    user_name : ['Sahil'],
+    user_password  : [''],
+    confirm_password : [''],
+    user_address : this.fb.group({
+    user_city : [''],
+    user_state  : [''],
+    user_postCode : [''],
     })
   });
+
+  // ----Using FormGroup method to create model for the Form---->
+
+  // registrationForm = new FormGroup({
+  //   user_name : new FormControl('Sahil'),
+  //   user_password  : new FormControl(''),
+  //   confirm_password : new FormControl(''),
+  //   user_address : new FormGroup({
+  //   user_city : new FormControl(''),
+  //   user_state  : new FormControl(''),
+  //   user_postCode : new FormControl(''),
+  //   })
+  // });
+
+
+
+  loadAllData(){
+    this.registrationForm.setValue({
+      user_name : 'Ronaldo',
+      user_password  : 'cris123',
+      confirm_password : 'cris123',
+      user_address : {
+        user_city : "Madrid",
+        user_state  : "Real Madrid",
+        user_postCode : "110034"
+      }
+    });
+  }
+
+
+
+  loadFewData(){
+    this.registrationForm.patchValue({
+      user_name : 'Messi',
+      user_password  : 'leo123',
+      confirm_password : 'leo123'
+    });
+  }
+
+
+
+
+  submitForm(){
+    console.log(this.registrationForm.value);
+  }
+
+
+  resetForm(){
+    //  To reset entire Form --->
+    //  this.registrationForm.reset();
+
+    // To reset only particular Field --->
+    this.registrationForm.get('user_name').reset();
+  }
+
+
 
 }
